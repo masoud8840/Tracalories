5068d7366cf098e4389c7a79bdea4e76e9aabd54
masoud8840 <masoudgharedaghi@yahoo.com>
2023-09-03 10:08:50 +0330

    added modal component and using it

---
+++ src/components/UI/Modal.vue
@@ -0,0 +1,56 @@
+<template>
+  <teleport to="#app">
+    <transition
+      leave-active-class="transition-all duration-500 opacity-0"
+      enter-active-class="transition-all duration-500"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+    >
+      <dialog
+        open="true"
+        class="modal"
+        v-if="props.isOpen"
+        @click="handleClose"
+      >
+        <transition
+          enter-active-class="transition-all duration-500"
+          enter-from-class="opacity-0"
+          enter-to-class="opacity-100"
+          leave-active-class="transition-all duration-500 opacity-0"
+        >
+          <section class="modal-inner" v-if="props.isOpen">
+            <div class="modal-header">
+              <h4 class="modal-heading">{{ props.modalTitle }}</h4>
+              <i class="fas fa-close text-xl cursor-pointer"></i>
+            </div>
+            <slot></slot>
+          </section>
+        </transition></dialog></transition
+  ></teleport>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+
+const props = defineProps({
+  modalTitle: {
+    type: String,
+    default: "Undefined",
+  },
+  isOpen: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emits = defineEmits(["close"]);
+function handleClose(e) {
+  if (e.target.localName === "dialog") {
+    emits("close", !props.isOpen);
+  }
+}
+const modalDelayedOpenStatus = ref(false);
+watch(props, (newVal) => {
+  modalDelayedOpenStatus.value = newVal.isOpen;
+});
+</script>
--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -6,11 +6,39 @@
         Tracalories
       </a>
       <ul class="navbar-menu">
-        <li><button class="menu-item">Set daily limit</button></li>
+        <li>
+          <button class="menu-item" @click="openModal">Set daily limit</button>
+        </li>
         <li>
           <button class="menu-item">Reset day</button>
         </li>
       </ul>
     </nav>
   </header>
+
+  <Modal
+    modal-title="Set Daily Limit"
+    :is-open="isModalOpen"
+    @close="closeModal"
+  >
+    <form class="modal-body flex flex-col gap-3" @submit.prevent="closeModal">
+      <label for="calories-limit">Daily calories limit</label>
+      <input id="calories-limit" type="text" placeholder="Calories" />
+      <button>Save</button>
+    </form>
+  </Modal>
 </template>
+
+<script setup>
+import Modal from "../UI/Modal.vue";
+import { ref } from "vue";
+const isModalOpen = ref(false);
+
+function openModal() {
+  isModalOpen.value = true;
+}
+
+function closeModal() {
+  isModalOpen.value = false;
+}
+</script>
